a8d6a95f73e4b864c135648ed1c9f5cd3444008e
songdengqiang <ai491154541@163.com>
2021-03-03 20:01:59 +0800

    知识图谱管理页面修改

--- src/components/Project/zb_system/kg_zb_mainPage.vue
+++ src/components/Project/zb_system/kg_zb_mainPage.vue
@@ -55,7 +55,7 @@
             </div>
           </li>
           <li class="list_input_li">
-            <h3>实体类：</h3>
+            <h3 title="可以为导入的实体进行类的规定！">实体类：</h3>
             <input
               type="text"
               id="district"
@@ -85,7 +85,7 @@
             </div>
           </li>
           <li class="list_input_li">
-            <h3>实体属性：</h3>
+            <h3 title="可以为导入的实体进行属性的添加！不同属性之间以分号隔开">实体属性：</h3>
             <input
               type="text"
               class="list_input_value"
@@ -129,6 +129,25 @@
               placeholder="entity"
               v-model="kg_head"
             />
+            <div class="dropdown">
+              <button
+                type="button"
+                class="btn dropdown-toggle"
+                data-toggle="dropdown"
+              ></button>
+              <div class="dropdown-menu dropdown-menu-right">
+                <a
+                  class="dropdown-item"
+                  id="dropdownitem"
+                  href="#"
+                  v-for="x in myKG11.nodes"
+                  :key="x.id"
+                  :title="x.name"
+                  @click="sendValue(x.name, 'name_head')"
+                  >{{ x.name }}</a
+                >
+              </div>
+            </div>
           </li>
           <li class="list_input_li">
             <h3>尾实体：</h3>
@@ -138,6 +157,25 @@
               placeholder="entity"
               v-model="kg_footer"
             />
+            <div class="dropdown">
+              <button
+                type="button"
+                class="btn dropdown-toggle"
+                data-toggle="dropdown"
+              ></button>
+              <div class="dropdown-menu dropdown-menu-right">
+                <a
+                  class="dropdown-item"
+                  id="dropdownitem"
+                  href="#"
+                  v-for="x in myKG11.nodes"
+                  :key="x.id"
+                  :title="x.name"
+                  @click="sendValue(x.name, 'name_foot')"
+                  >{{ x.name }}</a
+                >
+              </div>
+            </div>
           </li>
           <li class="list_input_li">
             <h3>实体关系：</h3>
@@ -147,6 +185,25 @@
               placeholder="relation"
               v-model="kg_relation"
             />
+            <div class="dropdown">
+              <button
+                type="button"
+                class="btn dropdown-toggle"
+                data-toggle="dropdown"
+              ></button>
+              <div class="dropdown-menu dropdown-menu-right">
+                <a
+                  class="dropdown-item"
+                  id="dropdownitem"
+                  href="#"
+                  v-for="x in kgReTyList"
+                  :key="x.id"
+                  :title="x"
+                  @click="sendValue(x, 'relation')"
+                  >{{ x }}</a
+                >
+              </div>
+            </div>
           </li>
           <li class="list_input_li">
             <div
@@ -165,40 +222,25 @@
             </div>
             <div
               class="list_input_btn"
-              title="导入多组知识"
-              @click="addSomeKg()"
+              title="基于关系进行查询，可以只是关系，也可以是实体带关系"
+              @click="searchKg()"
             >
-              导入
-            </div>
-          </li>
-        </ul>
-      </div>
-      <div class="zb_main_right_bar">
-        <ul class="list_input">
-          <li class="list_input_li">
-            <h3>关系类型：</h3>
-            <input type="text" class="list_input_value" placeholder="value" />
-          </li>
-          <li class="list_input_li">
-            <h3>关系名：</h3>
-            <input type="text" class="list_input_value" placeholder="value" />
-          </li>
-          <li class="list_input_li">
-            <h3>关系属性：</h3>
-            <input
-              type="text"
-              class="list_input_value"
-              placeholder="key_value;key_value"
-            />
-          </li>
-          <li class="list_input_li">
-            <div class="list_input_btn" title="删除某一个关系">删除</div>
-            <div class="list_input_btn" title="添加一个关系">添加</div>
-            <div class="list_input_btn" title="查询一个关系的所有实体">
               查询
             </div>
           </li>
         </ul>
+      </div>
+      <div class="zb_main_right_bar1">
+        <h4>其它辅助小功能</h4>
+        <div class="funList">
+          <button class="btn btn-outline-secondary">删除所有知识节点</button>
+          <button class="btn btn-outline-secondary">查询所有知识节点</button>
+          <button class="btn btn-outline-secondary">批量删除关系</button>
+          <button class="btn btn-outline-secondary">批量删除实体</button>
+          <button class="btn btn-outline-secondary">实体属性修改</button>
+          <button class="btn btn-outline-secondary">查询功能扩展</button>
+          <button class="btn btn-outline-secondary">实体关系修改</button>
+        </div>
       </div>
       <!-- 数据的导入窗口 -->
       <div class="inputFile" v-show="display_input">
@@ -280,7 +322,7 @@ export default {
       entityNum: 0,
       entity_labels: "common",
       entity_name: "",
-      entity_attr: "",
+      entity_attr: "color_green;",
       kg_head: "",
       kg_footer: "",
       kg_relation: "",
@@ -298,8 +340,8 @@ export default {
       //知识组变量
       kgEnTyList: [],
       kgReTyList: [],
-      KgListss:[],
-      kgEnL:[],
+      KgListss: [],
+      kgEnL: [],
     };
   },
   methods: {
@@ -382,6 +424,15 @@ export default {
         case "type":
           _this.entity_labels = x;
           break;
+        case "name_head":
+          _this.kg_head = x;
+          break;
+        case "name_foot":
+          _this.kg_footer = x;
+          break;
+        case "relation":
+          _this.kg_relation = x;
+          break;
       }
     }, //数据选择性传递
     inputClose() {
@@ -402,24 +453,26 @@ export default {
           break;
         case 2:
           document.getElementById("kgJson").click();
-          _this.display_input_num = 1;
+          _this.display_input_num = 2;
           break;
         case 3:
           document.getElementById("kgJson").click();
-          _this.display_input_num = 1;
+          _this.display_input_num = 3;
           break;
       }
     },
     submitKgE() {
       const _this = this;
       if (_this.display_input_num === 0) {
+        _this.KgListss = [];
+        _this.kgEnL = [];
         let kgTxt = document.getElementById("kgTxt").files[0];
         let render = new FileReader(); //新建一个文件读取器
         render.readAsText(kgTxt, "UTF-8"); //将读取的内容转化为文本
         render.onload = function (event) {
           let a = event.target.result.replace(/\([^\)]*\)/g, "");
           const dataList = a.split(/[(\r\n)\r\n]+/); //以换行符进行分割
-          const singleE = []
+          const singleE = [];
           for (let i of dataList) {
             i = i.replace(/^\s+|\s+$/g, ""); // 去除两端的空格
             if (i !== "") {
@@ -431,28 +484,116 @@ export default {
               kg.foot = splitD[1];
               kg.relation = splitD[2];
               _this.KgListss.push(kg);
-              const kgE = {}
-              kgE.name = splitD[0]
-              kgE.labels = _this.entity_labels
-              if(singleE.indexOf(splitD[0]) ===-1){
-                singleE.push(splitD[0])
-                _this.kgEnL.push(kgE)
+              const kgE = {};
+              kgE.name = splitD[0];
+              kgE.labels = _this.entity_labels;
+              let attrlist = _this.entity_attr.split(";");
+              for (let i of attrlist) {
+                if (i !== "") {
+                  let attrKey = i.split("_");
+                  kgE[attrKey[0]] = attrKey[1];
+                }
               }
-              const kgE1 = {}
-              kgE1.name = splitD[0]
-              kgE1.labels = _this.entity_labels
-              if(singleE.indexOf(splitD[1]) ===-1){
-                singleE.push(splitD[1])
-                _this.kgEnL.push(kgE1)
+              if (singleE.indexOf(splitD[0]) === -1) {
+                singleE.push(splitD[0]);
+                _this.kgEnL.push(kgE);
+              }
+              const kgE1 = {};
+              kgE1.name = splitD[1];
+              kgE1.labels = _this.entity_labels;
+              for (let i of attrlist) {
+                if (i !== "") {
+                  let attrKey = i.split("_");
+                  kgE1[attrKey[0]] = attrKey[1];
+                }
+              }
+              if (singleE.indexOf(splitD[1]) === -1) {
+                singleE.push(splitD[1]);
+                _this.kgEnL.push(kgE1);
               }
             }
           }
-          _this.entityNum = 0
-          _this.kgNum = 0
-          _this.entityNum = _this.kgEnL.length
-          _this.kgNum = _this.KgListss.length
-          console.log(_this.KgListss)
+          _this.entityNum = 0;
+          _this.kgNum = 0;
+          _this.entityNum = _this.kgEnL.length;
+          _this.kgNum = _this.KgListss.length;
+          console.log(_this.KgListss);
           //实体的上传
+          axios.post(_this.path + "/addManyKg1", _this.kgEnL).then((res) => {
+            if (res.data === "成功") {
+              alert(res.data);
+            }
+          });
+        };
+      } else if (_this.display_input_num === 1) {
+        _this.KgListss = [];
+        _this.kgEnL = [];
+        let kgJson = document.getElementById("kgJson").files[0];
+        let render = new FileReader(); //新建一个文件读取器
+        render.readAsText(kgJson, "UTF-8"); //将读取的内容转化为文本
+        render.onload = function (event) {
+          let data = JSON.parse(event.target.result); //将获取的数据转化为json格式
+          // console.log(data);
+          const singleE = [];
+          for (let x of data) {
+            if (singleE.indexOf(x.name) === -1) {
+              singleE.push(x.name);
+              _this.kgEnL.push(x);
+            }
+          }
+          _this.entityNum = 0;
+          _this.kgNum = 0;
+          _this.entityNum = _this.kgEnL.length;
+          _this.kgNum = _this.KgListss.length;
+          axios.post(_this.path + "/addManyKg1", _this.kgEnL).then((res) => {
+            if (res.data === "成功") {
+              alert(res.data);
+            }
+          });
+        };
+      } else if (_this.display_input_num === 2) {
+        _this.KgListss = [];
+        _this.kgEnL = [];
+        let kgJson = document.getElementById("kgJson").files[0];
+        let render = new FileReader(); //新建一个文件读取器
+        render.readAsText(kgJson, "UTF-8"); //将读取的内容转化为文本
+        render.onload = function (event) {
+          let data = JSON.parse(event.target.result); //将获取的数据转化为json格式
+          const singleE = [];
+          for (let x of data) {
+            _this.KgListss.push(x);
+            let attrlist = _this.entity_attr.split(";");
+            if (singleE.indexOf(x.head) === -1) {
+              singleE.push(x.head);
+              const kgE = {};
+              kgE.name = x.head;
+              kgE.labels = _this.entity_labels;
+              for (let i of attrlist) {
+                if (i !== "") {
+                  let attrKey = i.split("_");
+                  kgE[attrKey[0]] = attrKey[1];
+                }
+              }
+              _this.kgEnL.push(kgE);
+            }
+            if (singleE.indexOf(x.foot) === -1) {
+              singleE.push(x.foot);
+              const kgE1 = {};
+              kgE1.name = x.foot;
+              kgE1.labels = _this.entity_labels;
+              for (let i of attrlist) {
+                if (i !== "") {
+                  let attrKey = i.split("_");
+                  kgE1[attrKey[0]] = attrKey[1];
+                }
+              }
+              _this.kgEnL.push(kgE1);
+            }
+          }
+          _this.entityNum = 0;
+          _this.kgNum = 0;
+          _this.entityNum = _this.kgEnL.length;
+          _this.kgNum = _this.KgListss.length;
           axios.post(_this.path + "/addManyKg1", _this.kgEnL).then((res) => {
             if (res.data === "成功") {
               alert(res.data);
@@ -461,6 +602,13 @@ export default {
         };
       }
     },
+    submitKgR() {
+      const _this = this;
+      // console.log(_this.kgList);
+      axios.post(_this.path + "/addMangKg2", _this.KgListss).then((res) => {
+          alert(res.data);
+      });
+    },//上传知识之间的关系
 
     // 知识图谱的数据处理和绘制函数
     dealKgData() {
@@ -470,6 +618,12 @@ export default {
         // console.log(_this.kgEnTyList.indexOf(x.labels[0]))
         if (_this.kgEnTyList.indexOf(x.labels[0]) === -1) {
           _this.kgEnTyList.push(x.labels[0]);
+        }
+      }
+      for (let x of _this.myKG11.links) {
+        // console.log(x)
+        if (_this.kgReTyList.indexOf(x.relation) === -1) {
+          _this.kgReTyList.push(x.relation);
         }
       }
     }, //处理知识图数据，获取独立地知识节点类型
@@ -492,8 +646,10 @@ export default {
         objdata.labels = _this.entity_labels;
         let attrlist = _this.entity_attr.split(";");
         for (let i of attrlist) {
-          let attrKey = i.split("_");
-          objdata[attrKey[0]] = attrKey[1];
+          if (i !== "") {
+            let attrKey = i.split("_");
+            objdata[attrKey[0]] = attrKey[1];
+          }
         }
         //存入数据库
         axios
@@ -550,140 +706,26 @@ export default {
       _this.kgNum = 0;
       _this.display_input_num = 0;
     }, // 添加多组实体
-    addSomeKg() {
+    searchKg() {
       const _this = this;
-      _this.display_input = true;
-      _this.entityNum = 0;
-      _this.newEntityNum = 0;
-      _this.kgNum = 0;
-      _this.display_input_num = 1;
-    }, //添加多组知识数据
-    // submitKgE() {
-    //   if ((_this.display_input_num = 0)) {
-    //     const _this = this;
-    //     let kgJson = document.getElementById("kgJson").files[0];
-    //     let render = new FileReader(); //新建一个文件读取器
-    //     render.readAsText(kgJson, "UTF-8"); //将读取的内容转化为文本
-    //     render.onload = function (event) {
-    //       let result = JSON.parse(event.target.result); //读取文件内容
-    //       console.log(result);
-    //     };
-    //   } else {
-    //     const _this = this;
-    //     let kgJson = document.getElementById("kgJson").files[0];
-    //     let render = new FileReader(); //新建一个文件读取器
-    //     render.readAsText(kgJson, "UTF-8"); //将读取的内容转化为文本
-    //     render.onload = function (event) {
-    //       let result = JSON.parse(event.target.result); //读取文件内容
-    //       _this.kgNum = result.length;
-    //       let entityLists = [];
-    //       let newEntityList = [];
-    //       _this.kgList = [];
-    //       for (let i of result) {
-    //         let repead3 = 0;
-    //         // console.log(_this.myKG11.links)
-    //         for (let o of _this.myKG11.links) {
-    //           if (
-    //             i.header === o.target.name &&
-    //             i.footer === o.source.name &&
-    //             i.relation === o.relation
-    //           ) {
-    //             repead3 = 1;
-    //             break;
-    //           }
-    //         }
-    //         if (!repead3) {
-    //           let kgObj = {};
-    //           kgObj.head = i.header;
-    //           kgObj.foot = i.footer;
-    //           kgObj.relation = i.relation;
-    //           _this.kgList.push(kgObj);
-    //         }
-    //         let repead1 = 0;
-    //         let repead2 = 0;
-    //         for (let j of _this.myKG11.nodes) {
-    //           if (i.header === j.name) {
-    //             entityLists.push(j);
-    //             repead1 = 1;
-    //             break;
-    //           }
-    //         }
-    //         for (let k of _this.myKG11.nodes) {
-    //           if (i.footer === k.name) {
-    //             entityLists.push(k);
-    //             repead2 = 1;
-    //             break;
-    //           }
-    //         }
-    //         if (!repead1) {
-    //           if (newEntityList.length > 0) {
-    //             let num = 0;
-    //             for (let n of newEntityList) {
-    //               if (i.header === n.name) {
-    //                 num = 1;
-    //                 break;
-    //               }
-    //             }
-    //             if (!num) {
-    //               let obj = {};
-    //               obj.name = i.header;
-    //               obj.labels = "common";
-    //               newEntityList.push(obj);
-    //               entityLists.push(obj);
-    //             }
-    //           } else {
-    //             let obj = {};
-    //             obj.name = i.header;
-    //             obj.labels = "common";
-    //             newEntityList.push(obj);
-    //             entityLists.push(obj);
-    //           }
-    //         }
-    //         if (!repead2) {
-    //           if (newEntityList.length > 0) {
-    //             let num = 0;
-    //             for (let n of newEntityList) {
-    //               if (i.footer === n.name) {
-    //                 num = 1;
-    //                 break;
-    //               }
-    //             }
-    //             if (!num) {
-    //               let obj = {};
-    //               obj.name = i.footer;
-    //               obj.labels = "common";
-    //               newEntityList.push(obj);
-    //               entityLists.push(obj);
-    //             }
-    //           } else {
-    //             let obj = {};
-    //             obj.name = i.footer;
-    //             obj.labels = "common";
-    //             newEntityList.push(obj);
-    //             entityLists.push(obj);
-    //           }
-    //         }
-    //       } //数据的处理
-    //       _this.newEntityNum = newEntityList.length;
-    //       _this.entityNum = entityLists.length;
-    //       axios.post(_this.path + "/addManyKg1", newEntityList).then((res) => {
-    //         if (res.data === "成功") {
-    //           alert(res.data);
-    //           _this.$d3.select(".btn11").style("curser", "none");
-    //         }
-    //       });
-    //     };
-    //   }
-    // },
-    submitKgR() {
-      const _this = this;
-      console.log(_this.kgList);
-      axios.post(_this.path + "/addMangKg2", _this.KgListss).then((res) => {
-        if (res.data === "成功") {
-          alert(res.data);
-        }
+      let searchData = {}
+      searchData.head = _this.kg_head
+      searchData.foot = _this.kg_footer
+      searchData.relation = _this.kg_relation
+      axios.post(_this.path + "/searchKg1", searchData).then((res) => {
+          // console.log(res.data);
+          if(res.data === '查询失败！'){
+            alert(res.data)
+          }else{
+            let nodes = []  
+            _this.search_myKG11.links = res.data.links;
+            _this.search_myKG11.nodes = res.data.nodes;
+            console.log(_this.search_myKG11);
+            _this.myKG1.updataGraphs(_this.search_myKG11);
+          }
       });
-    },
+    }, //查询知识节点
+
   },
   mounted() {
     const _this = this;
